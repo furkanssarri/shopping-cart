8e48415ae84491820f8fe43bf8b5bc1733061f38
Furkan Sarı <furkansa@hotmail.com.tr>
2025-07-16 16:05:47 +0300

    feat: add `useData` as an abstracted fetch logic

---
+++ src/utils/useData.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+
+export default function useData() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `https://fakestoreapi.com/products`,
+          controller.signal,
+        );
+        if (!response.ok) {
+          setError("Error: cannot find the product");
+          setData(null);
+          return;
+        }
+        const json = await response.json();
+        setData(json);
+        setError(null);
+      } catch (errMessage) {
+        if (errMessage === "AbortError") {
+          console.log("Aborted");
+          setError("Aborted");
+          return;
+        }
+        setData(null);
+        setError(errMessage);
+        console.log(errMessage);
+        setIsLoading(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getData();
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  return {
+    data,
+    error,
+    isLoading,
+  };
+}
