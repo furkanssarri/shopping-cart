8229cdbfb06365f5c9c1b3915a26784ea3b1b08b
Furkan Sarı <furkansa@hotmail.com.tr>
2025-07-16 16:08:27 +0300

    feat: implement the extracted `useData` to shop
    the extracted fetch logic inside useData was implemented into the shop
    to fetch a list of products. not styled right now, will Soon™ be. (swh)

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,7 +3,7 @@ import styles from "./product.module.css";
 const Product = () => {
   return (
     <div className={styles.container}>
-      <h1>Product Page</h1>
+      <h1>Product Detail Page</h1>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,39 @@
 import styles from "./shop.module.css";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import useData from "../../utils/useData";
+import { useEffect } from "react";
+import ProductThumbNail from "../product-thumbnail/ProductThumbNail";
 
 const Shop = () => {
+  const { data, error, isLoading } = useData();
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  if (error) return <p>{error}</p>;
+
+  if (isLoading) return <p>Loading data...</p>;
+
   return (
     <div className={styles.container}>
-      <h1>Some products</h1>
+      <h1>Products</h1>
+      <ul>
+        {data &&
+          data.map((item) => (
+            <li key={item.id}>
+              <Link to={`/:${item.id}`}>
+                <ProductThumbNail
+                  title={item.title}
+                  price={item.price}
+                  description={item.description}
+                  image={item.image}
+                  rating={item.rating.rate}
+                />
+              </Link>
+            </li>
+          ))}
+      </ul>
       <Outlet />
     </div>
   );
